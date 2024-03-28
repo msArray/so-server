80a5bb24155e8939c0150cc17aa2818c372c4371
Kurita Masaya <masaya93@ogaki-tv.ne.jp>
2024-03-28 04:43:00 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,39 @@
-import { Hono } from 'hono';
-import { upgradeWebSocket } from 'hono/cloudflare-workers';
+import { Hono } from "hono";
+import { createBunWebSocket } from "hono/bun";
 
-const app = new Hono()
+const { upgradeWebSocket, websocket } = createBunWebSocket();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
+const PORT = 3000;
+
+app.get("/", (c) => {
+  return c.text("api");
+});
 
 app.get(
-  '/ws',
+  "/ws",
   upgradeWebSocket((c) => {
+    let intervalId: any;
     return {
+      onOpen(_event, ws) {
+        intervalId = setInterval(() => {
+          ws.send("あかさたな　はまやらわ");
+        }, 1000/30);
+      },
       onMessage(event, ws) {
-        console.log(`Message from client: ${event.data}`)
-        ws.send('Hello from server!')
+        console.log(`Message from client: ${event.data}`);
+        ws.send(`Echo: ${event.data}`);
       },
-      onClose: () => {
-        console.log('Connection closed')
+      onClose() {
+        clearInterval(intervalId);
       },
-    }
+    };
   })
-)
+);
 
-export default app
+Bun.serve({
+  fetch: app.fetch,
+  websocket,
+  port: PORT,
+});
+console.log(`Server started at ${PORT}`);
