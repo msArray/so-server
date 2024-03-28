f734600ced9ef17a2e013317afa372d36c881f01
Kurita Masaya <masaya93@ogaki-tv.ne.jp>
2024-03-28 03:38:16 +0000

    server sample

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# so-server
-so's server
+To install dependencies:
+```sh
+bun install
+```
+
+To run:
+```sh
+bun run dev
+```
+
+open http://localhost:3000
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Hono } from 'hono';
+import { upgradeWebSocket } from 'hono/cloudflare-workers';
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get(
+  '/ws',
+  upgradeWebSocket((c) => {
+    return {
+      onMessage(event, ws) {
+        console.log(`Message from client: ${event.data}`)
+        ws.send('Hello from server!')
+      },
+      onClose: () => {
+        console.log('Connection closed')
+      },
+    }
+  })
+)
+
+export default app
